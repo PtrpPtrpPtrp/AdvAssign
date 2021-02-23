476ab5f113f860ba5362cfea2056e9cdc99976cf
PtrpPtrpPtrp <6106021620052@fitm.kmutnb.ac.th>
2021-02-23 21:03:06 +0700

    Up to Git

--- PyRedis.py
+++ PyRedis.py
@@ -1,30 +1,60 @@
 import os
-import json
 import redis
 from flask import Flask,request,jsonify
 
+#
 app = Flask(__name__)
-db = redis.StrictRedis(
-        host='node11025-adv-17.app.ruk-com.cloud',
-        port=11169,
-        password='EGGars58847'
-        ,decode_responses=True)
+db=redis.StrictRedis(
+        host='node11042-advweb-172.app.ruk-com.cloud',#
+        port=11175,#
+        password='HPTfvf55686',#
+        decode_responses=True)
 
-
-@app.route('/',methods=['GET'])
-def show():
+@app.route('/',methods=['GET']) 
+def Show_Allmusic():
     name=db.keys()
     name.sort()
     req = []
-    for r in name :
-        req.append(db.hgetall(r))
+    for i in name :
+        req.append(db.hgetall(i))
     return jsonify(req)
-    #return 'Hello %s!' % name
 
-@app.route('/setname/<name>')
-def setname(name):
-    db.set('name',name)
-    return 'Name updated.'
+
+@app.route('/<Key>', methods=['GET'])
+def Show_Music(Key):
+    name = db.hgetall(Key)
+    return jsonify(name)
+
+
+@app.route('/Insert', methods=['POST'])
+def add_Music():
+    id = request.json['id']
+    type = request.json['type']
+    user = request.json['user']
+    #name = request.json['name']
+    #genre = request.json['episode']
+    user = {"id":id, "type":type, "user":user}
+    db.hmset(id,user)
+    return 'Insert Done!!!'
+
+
+@app.route('/Edit/<Key>', methods=['PUT'])
+def update_Music(Key):
+    #id = request.json['id']
+    type = request.json['type']
+    user = request.json['user']
+    #name = request.json['name']
+    #genre = request.json['episode']
+    user = {"id":Key, "type":type, "user":user}
+    db.hmset(Key,user)
+    return 'Update Done!!!'
+
+
+@app.route('/<Key>', methods=['DELETE'])
+def delete_Music(Key):
+    db.delete(Key)
+    return 'Delete Done!!!'
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=80)
